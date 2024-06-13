1bc4f1a9ba293252c615c21f306b7949ff794197
answad <mymhj122@gmail.com>
2024-06-14 01:13:46 +0900

    Color 추가

---
+++ JDS-component/src/main/java/com/example/jds_component/color/JDS.kt
@@ -0,0 +1,24 @@
+package com.example.jds_component.color
+
+import androidx.compose.ui.graphics.Color
+
+object JDS {
+    val MAIN = Color(0xFF1243F1)
+    val Black = Color(0xFF333333)
+    val WHITE = Color(0xFFFFFFFF)
+
+    val GRAY900 = Color(0xFF4F4F51)
+    val GRAY800 = Color(0xFF666669)
+    val GRAY700 = Color(0xFF828387)
+    val GRAY600 = Color(0xFF8F9094)
+    val GRAY500 = Color(0xFFA5A6A9)
+    val GRAY400 = Color(0xFFB4B5B7)
+    val GRAY300 = Color(0xFFDBDCDE)
+    val GRAY200 = Color(0xFFEFF0F2)
+    val GRAY100 = Color(0xFFF5F6F8)
+
+    val GRAY2 = Color(0xFFA3A3A3)
+    val GRAY1 = Color(0xFFD1D1D1)
+
+    val SYSTEM = Color(0xFFDF454A)
+}
