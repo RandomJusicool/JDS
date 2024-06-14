1061925b8c314904b82d2559700a242118128698
answad <mymhj122@gmail.com>
2024-06-14 16:00:07 +0900

    :lipstick: Image composable 함수 추가

---
+++ JDS-component/src/main/java/com/example/jds_component/image/CommunityFloatButtonImage.kt
@@ -0,0 +1,23 @@
+package com.example.jds_component.image
+
+import androidx.compose.foundation.Image
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jds_component.R
+
+@Composable
+fun CommunityFloatButtonImage(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.community_float_button_image),
+        contentDescription = null,
+        modifier = modifier,
+    )
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    CommunityFloatButtonImage()
+}
